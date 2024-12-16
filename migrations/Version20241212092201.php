cac31613fa5c025fdb8d219fa727e85f035bdb6f
ChristopherSaury <saurychristopher@gmail.com>
2024-12-16 11:13:37 +0100

    migration des tables fvdish et fvcategory

---
+++ migrations/Version20241212092201.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241212092201 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE fvdish CHANGE is_availabe is_available TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE fvdish CHANGE is_available is_availabe TINYINT(1) NOT NULL');
+    }
+}
