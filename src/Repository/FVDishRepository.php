9e7112a4b5146fde50b6156ae3369d080e55aabf
ChristopherSaury <saurychristopher@gmail.com>
2024-12-18 07:51:36 +0100

    ajout d'argument easybuilder pour n'afficher que les plats disponible

--- src/Repository/FVDishRepository.php
+++ src/Repository/FVDishRepository.php
@@ -16,11 +16,24 @@ class FVDishRepository extends ServiceEntityRepository
         parent::__construct($registry, FVDish::class);
     }
 
-    public function findDishes(int $value, int $max_result){
+    public function findDishes(int $value, int $max_result, bool $available = true){
         return $this->createQueryBuilder('d')
         ->andWhere('d.category = :val')
         ->setParameter('val', $value)
+        ->andWhere('d.isAvailable = :available')
+        ->setParameter('available', $available)
         ->setMaxResults($max_result)
+        ->getQuery()
+        ->getResult();
+    }
+
+    public function findDishBySlug($slug, bool $available = true){
+        return $this->createQueryBuilder('d')
+        ->join('d.category', 'c')
+        ->andWhere('c.slug = :slug')
+        ->setParameter('slug', $slug)
+        ->andWhere('d.isAvailable = :available')
+        ->setParameter('available', $available)
         ->getQuery()
         ->getResult();
     }
