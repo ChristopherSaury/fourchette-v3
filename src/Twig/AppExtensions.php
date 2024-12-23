8e5c7df04c1e67d0a214377571e2a847d5a31cea
ChristopherSaury <saurychristopher@gmail.com>
2024-12-23 07:57:31 +0100

    Ajout de GlobalInterface

--- src/Twig/AppExtensions.php
+++ src/Twig/AppExtensions.php
@@ -2,11 +2,19 @@
 
 namespace App\Twig;
 
+use App\Classe\Cart;
 use Twig\TwigFilter;
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 
-class AppExtensions extends AbstractExtension
+class AppExtensions extends AbstractExtension implements GlobalsInterface
 {
+    private $cart;
+
+    public function __construct(Cart $cart)
+    {
+        $this->cart = $cart;
+    }
     public function getFilters()
     {
         return [
@@ -16,5 +24,12 @@ class AppExtensions extends AbstractExtension
 
     public function formatPrice($number){
         return number_format($number,2,',') . ' €';
+    }
+
+    public function getGlobals(): array
+    {
+        return[
+            'getFullCartQty' => $this->cart->getFullCartQty(),
+        ];
     }
 }
