53b186a378b7429174a8e90867dc1121d8f0e3a6
ChristopherSaury <saurychristopher@gmail.com>
2024-12-31 09:23:01 +0100

    Création du crud easyadmin transporteur

---
+++ src/Controller/Admin/FVCarrierCrudController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\FVCarrier;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class FVCarrierCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return FVCarrier::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Transporteur')
+            ->setEntityLabelInPlural('Transporteurs');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name', 'Nom du transporteur')->setHelp('Nom du transporteur'),
+            TextareaField::new('description', 'Description du transporteur'),
+            NumberField::new('price', 'Prix T.T.C')->setHelp('Prix T.T.C du transporteur sans le sigle €'),
+        ];
+    }
+}
