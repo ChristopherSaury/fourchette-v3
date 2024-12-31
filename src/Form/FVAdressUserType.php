b3055cc5bc3f81246bbdc884e6435479af001f61
ChristopherSaury <saurychristopher@gmail.com>
2024-12-31 09:42:32 +0100

    restriction des adresse à la ville de Paris

--- src/Form/FVAdressUserType.php
+++ src/Form/FVAdressUserType.php
@@ -6,6 +6,7 @@ use App\Entity\FVAddress;
 use App\Entity\FVUser;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -29,17 +30,27 @@ class FVAdressUserType extends AbstractType
             'label' => 'Adresse :',
             'attr' => ['placeholder' => 'Saisissez l\'adresse de livraison']
             ])
-            ->add('postal', TextType::class,[
-            'label' => 'Code postal :',
-            'attr' => ['placeholder' => 'ex: 75107']
+            ->add('postal', ChoiceType::class,[
+                'choices' => [
+                    '75001' => '75001','75002' => '75002', '75003' => '75003',
+                    '75004' => '75004','75005' => '75005','75006' => '75006',
+                    '75007' => '75007','75008' => '75008','75009' => '75009',
+                    '75010' => '75010','75011' => '75011','75012' => '75012',
+                    '75013' => '75013','75014' => '75014','75015' => '75015',
+                    '75016' => '75016','75017' => '75017','75018' => '75018',
+                    '75019' => '75019','75020' => '75020',
+
+                ]
             ])
             ->add('city', TextType::class,[
+            'data' => 'Paris',
             'label' => 'Ville :',
-            'attr' => ['placeholder' => 'ex: Paris']  
+            'disabled' => true,
             ])
             ->add('country', CountryType::class,[
+            'data' => 'FR',
             'label' => 'Pays :',
-            'attr' => ['placeholder' => 'ex: France']
+            'disabled' => true,
             ])
             ->add('phone', TextType::class,[
             'label' => 'Numéro de téléphone :',
