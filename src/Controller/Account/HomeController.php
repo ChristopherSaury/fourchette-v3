9d7326e24560baa549d982b8d1fbb4c863fa526f
ChristopherSaury <saurychristopher@gmail.com>
2025-01-03 14:59:01 +0100

    AJout de l'historique des commandes

--- src/Controller/Account/HomeController.php
+++ src/Controller/Account/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Account;
 
+use App\Repository\FVOrderRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,10 +10,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/compte', name: 'app_account')]
-    public function index(): Response
+    public function index(FVOrderRepository $fVOrderRepository): Response
     {
+        $orders = $fVOrderRepository->findBy([
+            'user' => $this->getUser(),
+            'state' => [2,3,4],
+        ], ['createdAt' => 'DESC']);
+        
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'orders' => $orders
         ]);
     }
 }
