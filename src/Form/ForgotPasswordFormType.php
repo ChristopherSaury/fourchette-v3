23268409282c95b76b423183a895719e7b5b811c
ChristopherSaury <saurychristopher@gmail.com>
2025-01-13 09:38:50 +0100

    formulaire de demande réinitialisation du mot de passe

---
+++ src/Form/ForgotPasswordFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ForgotPasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class,[
+                'label' => 'Email du compte :',
+                'attr' =>['placeholder' => 'email@exemple.com'],
+                'help' => 'Vous recevrez un mot de passe provisoire'
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => 'Réinitialiser'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
