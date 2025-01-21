0a64b5e41fad909c3a3e184ddfa35bf298b5b73f
ChristopherSaury <saurychristopher@gmail.com>
2025-01-21 09:41:30 +0100

    Ajout de mail de bienvenue a la création de compte

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,31 +2,58 @@
 
 namespace App\Controller;
 
+use DateTime;
+use Mailjet\Client;
 use App\Entity\FVUser;
+use Mailjet\Resources;
 use App\Form\RegisterFVUserType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request,EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        $user = new FVUser; 
+        $user = new FVUser;
         $form = $this->createForm(RegisterFVUserType::class, $user);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setCreatedAt(new DateTime());
             $em->persist($user);
             $em->flush();
 
-            return $this->redirectToRoute('app_account');
+            $mj = new Client($_ENV['MJ_APIKEY_PUBLIC'], $_ENV['MJ_APIKEY_PRIVATE'], true, ['version' => 'v3.1']);
+
+            $body = [
+                'Messages' => [
+                    [
+                        'From' => [
+                            'Email' => $_ENV['ADMIN_EMAIL'],
+                            'Name' => "Saury Christopher"
+                        ],
+                        'To' => [
+                            [
+                                'Email' => $user->getEmail(),
+                                'Name' => $user->getFirstname()
+                            ]
+                        ],
+                        'TemplateID' => 6646381,
+                        'TemplateLanguage' => true,
+                        'Subject' => "Bienvenue sur La Fourchette Victorieuse",
+                    ]
+                ]
+            ];
+            $mj->post(Resources::$Email, ['body' => $body]);
+
+            return $this->redirectToRoute('app_login');
         }
 
-        return $this->render('register/index.html.twig',[
+        return $this->render('register/index.html.twig', [
             'registerForm' => $form->createView()
         ]);
     }
